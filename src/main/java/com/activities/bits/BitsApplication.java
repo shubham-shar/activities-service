d674797af08183de2a2d966f93647965010258e3
shubham sharma <2022mt93103@wilp.bits-pilani.ac.in>
2023-10-14 13:32:19 +0530

    initial commit

---
+++ src/main/java/com/activities/bits/BitsApplication.java
@@ -0,0 +1,13 @@
+package com.activities.bits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activities/bits/BitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activities.bits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
